3504eb51d486d68defe2177b87daa536ed2c3785
animevn <haanhth21@gmail.com>
2019-09-06 08:11:24 +0700

    create Actors

--- Flappy/GameScene.swift
+++ Flappy/GameScene.swift
@@ -8,6 +8,8 @@ class GameScene: SKScene {
     private var bird:Bird!
     private var pipes:Pipes!
     private var ground:Ground!
+    private var actors = [ActorsController]()
+    
     
     private func createView(){
         gameNode = SKSpriteNode(color: .clear, size: scene!.size)
@@ -16,26 +18,35 @@ class GameScene: SKScene {
         addChild(gameNode)
     }
     
+    private func createActors(){
+        
+        sky = Sky(image: "sky", parentNode: gameNode, zPosition: 0, duration: 150)
+        city = City(image: "city", parentNode: gameNode, zPosition: 1, duration: 90)
+        bird = Bird(images: ["bird1", "bird2"], parentNode: gameNode,
+                    position: CGPoint(x: screenSize().x/4, y: screenSize().y * 3/4) , zPosition: 2)
+        pipes = Pipes(topImg: "topPipe", bottomImg: "bottomPipe", parentNode: gameNode,
+                      zPosition: 3, duration: 6, durationForNewPipe: 3.3)
+        ground = Ground(image: "ground", parentNode: gameNode, zPosition: 4, duration: 5)
+    }
+    
+    private func addActorsToList(){
+        actors = [sky, city, bird, pipes, ground]
+    }
+    
+    private func startActors(){
+        actors.forEach{$0.start()}
+    }
+    
+    private func stopActors(){
+        actors.forEach{$0.stop()}
+    }
+    
     override func didMove(to view: SKView) {
         backgroundColor = .green
         createView()
-        
-        sky = Sky(image: "sky", parentNode: gameNode, zPosition: 0, duration: 150)
-        sky.start()
-        
-        city = City(image: "city", parentNode: gameNode, zPosition: 1, duration: 90)
-        city.start()
-        
-        bird = Bird(images: ["bird1", "bird2"], parentNode: gameNode,
-                    position: CGPoint(x: screenSize().x/4, y: screenSize().y * 3/4) , zPosition: 2)
-        bird.start()
-        
-        pipes = Pipes(topImg: "topPipe", bottomImg: "bottomPipe", parentNode: gameNode,
-                      zPosition: 3, duration: 6, durationForNewPipe: 3.3)
-        pipes.start()
-        
-        ground = Ground(image: "ground", parentNode: gameNode, zPosition: 4, duration: 5)
-        ground.start()
+        createActors()
+        addActorsToList()
+        startActors()
     }
     
     
